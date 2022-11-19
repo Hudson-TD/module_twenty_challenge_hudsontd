f1b46ee3065141880c3a24cd18f1aa245d4b8b20
Tyler Hudson <tylerhudson96@gmail.com>
2022-11-19 13:16:18 -0500

    importing components into main App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
+import Header from "./components/Header";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div>In Development</div>;
+  return (
+    <>
+      <Navigation />
+      <Header />
+      <Project />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
