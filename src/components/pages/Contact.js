c28e7ce3b4e6010f8733b8b5f4400cf13f702b21
Tyler Hudson <tylerhudson96@gmail.com>
2022-11-20 13:38:35 -0500

    Updated contact form in place

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,31 +1,69 @@
 import React from "react";
 import {
+  Box,
+  Button,
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
   Input,
-  Box,
-  Center,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+  useColorModeValue,
+  VStack,
 } from "@chakra-ui/react";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsPerson } from "react-icons/bs";
 
 export default function Contact() {
   return (
-    <>
-      <Center>
-        <Box mt={12}>
-          <FormControl isRequired>
-            <FormLabel>First Name:</FormLabel>
-            <Input type="text" />
-            <FormLabel>Last Name:</FormLabel>
-            <Input type="text" />
-            <FormLabel>Email Address:</FormLabel>
-            <Input type="email" />
-            <FormLabel>Message:</FormLabel>
-            <Input type="email" />
-          </FormControl>
-        </Box>
-      </Center>
-    </>
+    <Box
+      bg={useColorModeValue("white", "gray.700")}
+      borderRadius="lg"
+      p={8}
+      color={useColorModeValue("gray.700", "whiteAlpha.900")}
+    >
+      <VStack spacing={5}>
+        <FormControl isRequired>
+          <FormLabel>Name</FormLabel>
+
+          <InputGroup>
+            <InputLeftElement children={<BsPerson />} />
+            <Input type="text" name="name" placeholder="Your Name" />
+          </InputGroup>
+        </FormControl>
+
+        <FormControl isRequired>
+          <FormLabel>Email</FormLabel>
+
+          <InputGroup>
+            <InputLeftElement children={<MdOutlineEmail />} />
+            <Input type="email" name="email" placeholder="Your Email" />
+          </InputGroup>
+        </FormControl>
+
+        <FormControl isRequired>
+          <FormLabel>Message</FormLabel>
+
+          <Textarea
+            name="message"
+            placeholder="Your Message"
+            rows={6}
+            resize="none"
+          />
+        </FormControl>
+
+        <Button
+          colorScheme="purple"
+          bg="purple.400"
+          color="white"
+          _hover={{
+            bg: "purple.500",
+          }}
+          isFullWidth
+        >
+          Send Message
+        </Button>
+      </VStack>
+    </Box>
   );
 }
